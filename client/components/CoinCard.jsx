e5dc3a0c745a094e515d4e0bafc5e81ff909217b
fraisai <r.uddin292@gmail.com>
2024-01-16 17:15:12 -0500

    added temporary error page

--- client/components/CoinCard.jsx
+++ client/components/CoinCard.jsx
@@ -15,7 +15,9 @@ const CoinCard = ({
   const largeImg = useRef({});
   let item = <></>;
   function handleClick(e) {
-    console.log("e", e)
+    console.log("e", e);
+    e.preventDefault();
+    
   }
   
   if (index === 0) {
@@ -60,8 +62,9 @@ const CoinCard = ({
 
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Market Price: {price}</p>
 
+              {/* Add to Investments Button */}
               <button onClick={handleClick}>
-                <svg width="40px" height="36px" viewBox="1.5 2 30 19" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#1a56db"><g id="SVGRepo_bgCarrier" strokeWidth="2"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <circle cx="12" cy="12" r="10" stroke="#1A56DB" strokeWidth="1.5"></circle> <path d="M15 12L12 12M12 12L9 12M12 12L12 9M12 12L12 15" stroke="#1a56db" strokeWidth="1.5" strokeLinecap="round"></path> </g></svg>
+                <svg width="40px" height="36px" viewBox="1.5 2 30 19" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#1a56db"><g id="SVGRepo_bgCarrier" strokeWidth="2"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <circle cx="12" cy="12" r="10" stroke="#1A56DB" strokeWidth="1.5"></circle> <path d="M15 12L12 12M12 12L9 12M12 12L12 9M12 12L12 15" stroke="#1a56db" strokeWidth="1.5" strokeLinecap="round"></path> </g></svg>
               </button>
             </div>
           </div>
--- client/components/dashboard-charts/OneWeekChart.jsx
+++ client/components/dashboard-charts/OneWeekChart.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import ErrorPage from '../ErrorPage'
 
 const OneWeekChart = ({ name, chartData }) => {
   return (
-    <div style={{maxHeight: '300px'}}>OneWeek</div>
+    <div style={{maxHeight: '300px'}}><ErrorPage /></div>
   )
 }
 

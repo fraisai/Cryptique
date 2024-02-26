1eb287e5dcab83daedae65e07389e77bb03c5e70
fraisai <r.uddin292@gmail.com>
2024-02-25 22:13:07 -0500

    added typescript return type

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -3,11 +3,10 @@ import bcrypt from 'bcrypt';
 import { User } from '../models/userModel';
 const SALT_WORK_FACTOR = 10;
 
-export const registerController = async (req: Request, res: Response, next: NextFunction) => {
+export const registerController = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { password, username, email } = req.body;
     try {
-        // hash password
-        const hashedPassword = await bcrypt.hash(password, SALT_WORK_FACTOR);
+        const hashedPassword = await bcrypt.hash(password, SALT_WORK_FACTOR); // hash password
         const newUser = await new User({
             username: username,
             email: email,
@@ -24,7 +23,6 @@ export const registerController = async (req: Request, res: Response, next: Next
             res.status(200).redirect('/build'); // meta, amazon - these companies send a 200 status code and then redirect user to a different page if same email is entered
             // res.status(200).redirect('back'); => in Express 4.x, use 'back' to automatically redirect back to the page the request came from
         }
-        
         return next();
     } catch (error) {
         console.log("ERROR in AUTHCONTROLLER FOR REGISTER", error)
